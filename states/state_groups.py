dc50c5404621d2f962440281b2a493e6b54792b7
Chenger1 <exs2199@gmail.com>
2021-08-14 21:51:06 +0300

    Add states

---
+++ states/state_groups.py
@@ -0,0 +1,22 @@
+from aiogram.dispatcher.filters.state import  State, StatesGroup
+
+
+class CreateTableState(StatesGroup):
+    starter = State()
+    name = State()
+    field = State()
+    save = State()
+
+
+class CreateRow(StatesGroup):
+    starter = State()
+    name = State()
+    save = State()
+
+
+class Transaction(StatesGroup):
+    starter = State()
+    transaction_type = State()
+    amount = State()
+    category = State()
+    save = State()
