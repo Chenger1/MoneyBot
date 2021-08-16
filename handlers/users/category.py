8db0163c445c7b88ce4df3742a146f2180bfd2c5
Chenger1 <exs2199@gmail.com>
2021-08-16 16:51:29 +0300

    Handler to create category

--- handlers/users/category.py
+++ handlers/users/category.py
@@ -4,17 +4,19 @@ from aiogram.dispatcher.filters.builtin import Text
 
 from loader import dp
 
+from states.state_groups import CreateCategory
 
+from keyboards.default import keyboards
 from keyboards.inline import keyboards as inline_keyboards
 from keyboards.dispatcher import dispatcher
 
-from db.models import Category
+from db.models import Category, User
 
 from typing import Optional
 
 
 async def list_categories(user_id: int) -> Optional[types.InlineKeyboardMarkup]:
-    instances = await Category.filter(user__pk=user_id)
+    instances = await Category.filter(user__user_id=user_id)
     if not instances:
         return None
     keyboard = await inline_keyboards.get_list(instances, 'category_detail')
@@ -37,6 +39,28 @@ async def list_category_handler(message: types.Message):
     await message.answer('Categories:', reply_markup=keyboard)
 
 
-@dp.message_handler(Text(equals=['Create category']))
-async def create_category_handler(message: types.Message, state: FSMContext):
-    pass
+@dp.message_handler(Text(equals=['Stop']), state=CreateCategory)
+async def category_back(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        await state.finish()
+        keyboard, path = await dispatcher('LEVEL_2_CATEGORY')
+        data['path'] = path
+    await message.answer('Creating category has been canceled', reply_markup=keyboard)
+
+
+@dp.message_handler(Text(equals=['Create Category']))
+async def create_category_handler(message: types.Message):
+    await CreateCategory.starter.set()
+    await message.answer('Add category name', reply_markup=keyboards.stop_create_object_button)
+    await CreateCategory.name.set()
+
+
+@dp.message_handler(state=CreateCategory.name)
+async def set_category_name(message: types.Message, state: FSMContext):
+    user = await User.get(user_id=message.from_user.id)
+    await Category.create(name=message.text, user=user)
+    async with state.proxy() as data:
+        await state.finish()
+        keyboard, path = await dispatcher('LEVEL_2_CATEGORY')
+        data['path'] = path
+    await message.answer('Category has been created', reply_markup=keyboard)
